66c86a995880f6573ed92920e4a00c81e6b988e5
Oleg Kossoy <oleg.kossoy@sap.com>
2013-09-23 00:08:54 +0300

    initial

---
+++ client/main.js
@@ -0,0 +1,40 @@
+Meteor.subscribe('items');
+
+Template.itemsList.helpers({
+    items: function () {
+        return Items.find();
+    }
+});
+
+Template.rowItem.helpers({
+    unitPrice: function () {
+        return (this.price / this.quantity * 0.5).toFixed(2);
+    }
+});
+
+Template.itemPage.helpers({
+        currentItem: function () {
+            return Items.findOne(Session.get('currentItemId'));
+        }
+    }
+);
+
+
+Template.itemSubmit.events({
+    "submit form": function (e) {
+        e.preventDefault();
+
+        var item = {
+            title: $(e.target).find("[name=title]").val(),
+            quantity: $(e.target).find("[name=quantity]").val(),
+            price: $(e.target).find("[name=price]").val()
+        };
+
+        Meteor.call('item', item, function (error, id) {
+            if (error) {
+                return alert(error.reason);
+            }
+            Meteor.Router.to("itemPage", id);
+        });
+    }
+});
