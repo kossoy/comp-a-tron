f1b4881ba062c057c7a27b33b0100918b619565c
Oleg Kossoy <oleg.kossoy@sap.com>
2013-11-21 13:44:03 +0200

    .meteor folder

--- client/main.js
+++ client/main.js
@@ -1,41 +1,43 @@
 Meteor.subscribe('items');
 
 Template.itemsList.helpers({
-    items: function () {
-        var items = Items.find().fetch()
-        return _(items).sortBy(function(obj){ return parseFloat(obj.price/obj.quantity); });
-    }
+  items: function () {
+    var items = Items.find().fetch()
+    return _(items).sortBy(function (obj) {
+      return parseFloat(obj.price / obj.quantity);
+    });
+  }
 });
 
 Template.rowItem.helpers({
-    unitPrice: function () {
-        return (this.price / this.quantity * 0.5).toFixed(2);
-    }
+  unitPrice: function () {
+    return (this.price / this.quantity * 0.5).toFixed(2);
+  }
 });
 
 Template.itemPage.helpers({
-        currentItem: function () {
-            return Items.findOne(Session.get('currentItemId'));
-        }
+    currentItem: function () {
+      return Items.findOne(Session.get('currentItemId'));
     }
+  }
 );
 
 
 Template.itemSubmit.events({
-    "submit form": function (e) {
-        e.preventDefault();
+  "submit form": function (e) {
+    e.preventDefault();
 
-        var item = {
-            title: $(e.target).find("[name=title]").val(),
-            quantity: $(e.target).find("[name=quantity]").val(),
-            price: $(e.target).find("[name=price]").val()
-        };
+    var item = {
+      title: $(e.target).find("[name=title]").val(),
+      quantity: $(e.target).find("[name=quantity]").val(),
+      price: $(e.target).find("[name=price]").val()
+    };
 
-        Meteor.call('item', item, function (error, id) {
-            if (error) {
-                return alert(error.reason);
-            }
-            Meteor.Router.to("itemPage", id);
-        });
-    }
+    Meteor.call('item', item, function (error, id) {
+      if (error) {
+        return alert(error.reason);
+      }
+      Meteor.Router.to("itemPage", id);
+    });
+  }
 });
